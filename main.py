95896456eca03a52a71a2b3b26a049514a3a0558
natatchikhinashvili <natanatuka33@gmail.com>
2023-11-29 12:51:57 +0400

    feat: logger

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import pcapy
+import logging
 
 from Config import Config
 from PycapParser import Parsepycap
 from Managedata import ManageData
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%{levelname}s:%(name)s:%(message)s')
+
+file_handler = logging.FileHandler('analyzer_log.log')
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
 
 
 def main():
@@ -21,6 +33,7 @@ def main():
 
         unpacked = Parsepycap.unpackheader(packet)
         decoded = Parsepycap.decodeInfo(*unpacked)
+        logger.info("Interface {}, Timestamp: {}, Packet Length: {} bytes,\n src mac: {}, dst_mac: {}, arp operation: {},\n src ip: {}, dst ip: {}".format(interface, header["ts"], header["len"], *decoded))
         myfile = manageData.write_to_file("Interface {}, Timestamp: {}, Packet Length: {} bytes ,\n src mac: {}, dst_mac: {}, arp operation: {},\n src ip: {}, dst ip: {} \n".format(interface, header.getts(), header.getlen(), *decoded))
         runtimecount+=1
     manageData.upload_to_gcs(myfile)
