ff73858d869cc7224e1cc8db3a1d92d22840dfff
natatchikhinashvili <natanatuka33@gmail.com>
2023-12-05 11:49:35 +0400

    feat: read config file only once

--- Config.py
+++ Config.py
@@ -4,6 +4,10 @@ import configparser
 config = configparser.ConfigParser()
 
 class Config:
+    def __init__(self, conf_file):
+        config.read(conf_file)
+        sections = config.sections()
+        
     def get_conf_file(self):
         try:
             parser = argparse.ArgumentParser()
@@ -15,26 +19,20 @@ class Config:
         except FileNotFoundError:
             print('File not implemented')
 
-    def read_interface(self, conf_file):
+    def read_interface(self):
         try:
-            config.read(conf_file)
-            sections = config.sections()
-            return config[sections['interface']][sections['interface']]
+            return config[self.sections['interface']][self.sections['interface']]
         except NotImplementedError:
             print("Interface field is not implemented in conf file")
     
-    def get_log_file(self, conf_file):
+    def get_log_file(self):
         try:
-            config.read(conf_file)
-            sections = config.sections()
-            return config[sections['logging']][sections['logging']]
+            return config[self.sections['logging']][self.sections['logging']]
         except NotImplementedError:
             print("logfile field is not implemented in conf file")
         
-    def get_run_times(self, conf_file):
+    def get_run_times(self):
         try:
-            config.read(conf_file)
-            sections = config.sections()
-            return config[sections['runtimes']][sections['runtimes']]
+            return config[self.sections['runtimes']][self.sections['runtimes']]
         except NotImplementedError:
             print("runtimes field is not implemented in conf file")
