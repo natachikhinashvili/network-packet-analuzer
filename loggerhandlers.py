e52d9d5aaaa38fc00fa9d27a148f0406e8955929
natatchikhinashvili <natanatuka33@gmail.com>
2023-12-05 12:03:50 +0400

    feat: logging formatter log timestamp linenumber and filename

--- loggerhandlers.py
+++ loggerhandlers.py
@@ -3,7 +3,7 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-formatter = logging.Formatter('%{levelname}s:%(name)s:%(message)s')
+formatter = logging.Formatter('%(lineno)d:%(filename)s:%(asctime)s:%{levelname}s:%(name)s:%(message)s')
 
 file_handler = logging.FileHandler('analyzer_log.log')
 file_handler.setFormatter(formatter)
