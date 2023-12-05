2eed553a12124735f2c1db54756acd7a41cbb5e4
natatchikhinashvili <natanatuka33@gmail.com>
2023-12-05 12:33:20 +0400

    feat: change print to logging

--- signalhandler.py
+++ signalhandler.py
@@ -1,4 +1,5 @@
+from loggerhandlers import logger
 def stop_handler(signum, frame, done):
     done = False
-    print("signum {}, frame: {}".format(signum, frame))
+    logger.warn("signum {}, frame: {}".format(signum, frame))
     return done
